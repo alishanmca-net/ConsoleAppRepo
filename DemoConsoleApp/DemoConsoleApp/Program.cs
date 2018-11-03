f238ebe8f29939c071d711708591a4721d3617e0
alishanmca-net <alishanmca.net@gmail.com>
2018-11-03 21:10:56 +0530

    Change in Program.cs

--- DemoConsoleApp/DemoConsoleApp/Program.cs
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -11,7 +11,7 @@ namespace DemoConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
-
+            Console.WriteLine("Hello friends");
         }
     }
 }
