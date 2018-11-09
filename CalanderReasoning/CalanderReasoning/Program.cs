a267b3418e6c116b0bbaa62c7eb3222c4c5ec494
alishanmca-net <alishanmca.net@gmail.com>
2018-11-10 00:08:12 +0530

    Change in Calander Program.cs

--- CalanderReasoning/CalanderReasoning/Program.cs
+++ CalanderReasoning/CalanderReasoning/Program.cs
@@ -6,7 +6,27 @@ namespace CalanderReasoning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            obj.readdata();
+            obj.leap();
+        }
+        int y;
+        public void readdata()
+        {
+            Console.WriteLine("Enter the Year in Four Digits : ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public void leap()
+        {
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", y);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", y);
+            }
+            Console.Read();
         }
     }
 }
