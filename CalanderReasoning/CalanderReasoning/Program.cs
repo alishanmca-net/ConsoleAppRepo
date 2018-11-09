ba3995d56d6e3196765c24916f6f21380b35346a
alishanmca-net <alishanmca.net@gmail.com>
2018-11-09 23:57:53 +0530

    Added new CalanderReasoning APP

---
+++ CalanderReasoning/CalanderReasoning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalanderReasoning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
